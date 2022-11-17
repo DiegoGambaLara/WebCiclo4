3f3cb7b98236e9abaec3678ae9718f1c0b478b73
DiegoGambaLara <diego.gamba1010@gmail.com>
2022-11-16 20:05:45 -0500

    CRUD completo

--- src/app/modulos/servicios/edit/edit.component.ts
+++ src/app/modulos/servicios/edit/edit.component.ts
@@ -18,6 +18,7 @@ export class EditComponent implements OnInit {
     private route: ActivatedRoute) { }
 
     fgValidacion = this.fb.group({
+      id: ['', [Validators.required]],
       origen: ['', [Validators.required]],
       destino: ['', [Validators.required]],
       fecha: ['', [Validators.required]],
@@ -34,6 +35,7 @@ export class EditComponent implements OnInit {
   getWithId(id: string){
     this.servicioService.getWithId(id).subscribe((data: ServicioModel) => {
       console.log(data)
+      this.fgValidacion.controls["id"].setValue(id)
       this.fgValidacion.controls["origen"].setValue (data.origen as string)
       this.fgValidacion.controls["destino"].setValue (data.destino as string)
       this.fgValidacion.controls["fecha"].setValue (data.fecha as string)
@@ -45,6 +47,7 @@ export class EditComponent implements OnInit {
 
   edit(){
     let servicio = new ServicioModel();
+    servicio.id = this.fgValidacion.controls["id"].value as string;
     servicio.origen = this.fgValidacion.controls["origen"].value as string;
     servicio.destino = this.fgValidacion.controls["destino"].value as string;
     servicio.fecha = this.fgValidacion.controls["fecha"].value as string;
